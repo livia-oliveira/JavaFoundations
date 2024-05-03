9123697e029f28e4400b57ac0b4674536885a0ba
livia-oliveira <livia0liveiraslv93@gmail.com>
2024-05-02 23:37:28 -0300

    Add element to the end of the array

---
+++ src/study_materials/lessons/data_structure/class1/TestVector.java
@@ -0,0 +1,11 @@
+package study_materials.lessons.data_structure.class1;
+
+public class TestVector {
+  public static void main(String[] args) {
+    Vector vector = new Vector(2);
+
+      vector.add("Elemento 1");
+      vector.add("Elemento 2");
+      vector.add("Elemento 3");
+  }
+}
